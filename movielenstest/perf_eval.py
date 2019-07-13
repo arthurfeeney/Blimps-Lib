8d43014b243614c3a936c1cffa001a4aba7bbb1b
arthurfeeney <afeeney@trinity.edu>
2019-07-12 22:03:04 -0500

    mips_recall test better optimized from improvements to k_probe

--- movielenstest/perf_eval.py
+++ movielenstest/perf_eval.py
@@ -6,31 +6,43 @@ import numpy as np
 # are put in the top k of N additional, unrated movies.
 #
 
+
 def find_five_rating(user_ratings, mean_rating):
     # return the indices of all 5-star ratings.
     # user_ratings should be a list.
-    indices = [i for i in range(user_ratings.size) if user_ratings[i] == 5 - mean_rating]
+    indices = [
+        i for i in range(user_ratings.size)
+        if user_ratings[i] == 5 - mean_rating
+    ]
     return indices
+
 
 def get_k_unrated(k, user_ratings):
     # randomly select k items (i.e., 1000 used by pureSVD) that are
     # unrated by the user.
-    unrated_indices = [idx for idx in range(user_ratings.size) if user_ratings[idx] == 0]
-    return np.random.choice(unrated_indices,
-                            size=k,
-                            replace=False) # won't select the same index many times
+    unrated_indices = [
+        idx for idx in range(user_ratings.size) if user_ratings[idx] == 0
+    ]
+    return np.random.choice(
+        unrated_indices, size=k,
+        replace=False)  # won't select the same index many times
 
-def random_item_ranked_list(k, user_ratings, five_star_idx, user_factors, item_factors):
+
+def random_item_ranked_list(k, user_ratings, five_star_idx, user_factors,
+                            item_factors):
     unrated_indices = get_k_unrated(k, user_ratings)
-    unrated_item_factors = item_factors[:,unrated_indices] # column matrix
+    unrated_item_factors = item_factors[:, unrated_indices]  # column matrix
     #ratings = user_factors.dot(unrated_item_factors)
-    ratings = user_ratings.dot(item_factors.transpose()).dot(unrated_item_factors)
-    five_star_rating = user_ratings.dot(item_factors.transpose()).dot(item_factors[:,five_star_idx])
+    ratings = user_ratings.dot(
+        item_factors.transpose()).dot(unrated_item_factors)
+    five_star_rating = user_ratings.dot(item_factors.transpose()).dot(
+        item_factors[:, five_star_idx])
     #ratings = user_factors.dot(unrated_item_factors)
     #five_star_rating = user_factors.dot(item_factors[:,five_star_idx])
     ratings.sort()
     print(ratings)
     return five_star_rating, ratings
+
 
 def hit(N, five_star_rating, ratings):
     # if five_star_rating is larger than the smallest item in the top N,
@@ -42,9 +54,11 @@ def hit(N, five_star_rating, ratings):
     else:
         return [five_star_rating > ratings[-n] for n in N]
 
+
 #
 # Precision and Recall for the exact case:
 #
+
 
 def user_recall(k, N, user_ratings, user_factors, item_factors, mean_rating):
     # computes the precision for a single user.
@@ -53,7 +67,7 @@ def user_recall(k, N, user_ratings, user_factors, item_factors, mean_rating):
 
     # if a user never gives something five stars, returning 1 effectively
     # "ignores" them.
-    if(len(five_star_indices) == 0):
+    if (len(five_star_indices) == 0):
         return 0
 
     total_hits = 0
@@ -63,8 +77,12 @@ def user_recall(k, N, user_ratings, user_factors, item_factors, mean_rating):
         total_hits += hit(N, fsr, ratings)
     return total_hits / len(five_star_indices)
 
-def user_precision(k, N, user_ratings, user_factors, item_factors, mean_rating):
-    return user_recall(k, N, user_ratings, user_factors, item_factors, mean_rating) / N
+
+def user_precision(k, N, user_ratings, user_factors, item_factors,
+                   mean_rating):
+    return user_recall(k, N, user_ratings, user_factors, item_factors,
+                       mean_rating) / N
+
 
 def recall(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating):
 
@@ -74,11 +92,12 @@ def recall(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating):
 
     total_hits = dict([(n, 0) for n in N])
     for (useridx, movieidx, rating) in test_ratings:
-        assert rating == 5 - mean_rating # all test ratings are 5 - mean
+        assert rating == 5 - mean_rating  # all test ratings are 5 - mean
         user_ratings = review_matrix_csr[useridx].toarray()[0]
         unrated_indices = get_k_unrated(k, user_ratings)
         unrated_item_factors = item_factors[unrated_indices]
-        ratings = user_ratings.dot(item_factors).dot(unrated_item_factors.transpose())
+        ratings = user_ratings.dot(item_factors).dot(
+            unrated_item_factors.transpose())
         five_star_rating = user_ratings\
             .dot(item_factors)\
             .dot(item_factors[int(movieidx)])
@@ -93,40 +112,50 @@ def recall(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating):
     return total_hits
 
 
-def precision(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating):
+def precision(k, N, test_ratings, item_factors, review_matrix_csr,
+              mean_rating):
     if isinstance(N, int):
         # make N iterable if it is an int.
         N = [N]
-    rec = recall(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating)
+    rec = recall(k, N, test_ratings, item_factors, review_matrix_csr,
+                 mean_rating)
     for key in rec:
         rec[key] /= key
     return rec
 
-def recall_and_precision(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating):
+
+def recall_and_precision(k, N, test_ratings, item_factors, review_matrix_csr,
+                         mean_rating):
     # dont have to compute recall and precision separately. This avoids some
     # unnecessary computations.
     if isinstance(N, int):
         # make N iterable if it is an int.
         N = [N]
-    rec = recall(k, N, test_ratings, item_factors, review_matrix_csr, mean_rating)
+    rec = recall(k, N, test_ratings, item_factors, review_matrix_csr,
+                 mean_rating)
     prec = dict([(key, rec[key] / key) for key in rec])
     return rec, prec
+
 
 #
 # Precision and Recall for MIPS.
 # Uses method described in "on Sym and Asym LSH paper."
 #
 
+
 def topk_inner(k, inner):
     # compute the topk largest inner products.
     indices = (-inner).argsort()[:k]
     return inner[indices], indices
 
+
 def fraction_intersect(l1, l2):
     # computes the fraction of elements in l2 that are also in l1.
     return len([a for a in l2 if a in l1]) / len(l1)
 
-def MIPS_recall(k, test_ratings, item_factors, nr_table, review_matrix_csr, mean_rating):
+
+def MIPS_recall(k, test_ratings, item_factors, nr_table, review_matrix_csr,
+                mean_rating):
     # computes the average overlap of topk ratings on the test set.
     total_recall = 0
     total_hits = 0
@@ -135,17 +164,18 @@ def MIPS_recall(k, test_ratings, item_factors, nr_table, review_matrix_csr, mean
         ctr += 1
         print(ctr)
 
-
         assert rating == 5 - mean_rating
         user_ratings = review_matrix_csr[useridx].toarray()[0]
 
         # get the true topk.
-        true_topk, true_idx = topk_inner(k, user_ratings.dot(item_factors).dot(item_factors.transpose()))
+        true_topk, true_idx = topk_inner(
+            k,
+            user_ratings.dot(item_factors).dot(item_factors.transpose()))
 
         # probe k from the nr table.
         query = user_ratings.dot(item_factors)
-        query /= np.linalg.norm(query) # queries are unit vectors
-        data, tracker = nr_table.k_probe(k, query, int(1000))
+        query /= np.linalg.norm(query)  # queries are unit vectors
+        data, tracker = nr_table.k_probe(k, query, int(5))
 
         if data:
             _, approx_idx = zip(*data)
