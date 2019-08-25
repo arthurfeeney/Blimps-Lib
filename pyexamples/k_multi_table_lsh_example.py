bb208819cd46b75719cc20f7fe6d1d1f746673b9
arthurfeeney <afeeney@trinity.edu>
2019-08-25 13:29:07 -0500

    added k_probe_approx to pyexamples and updated README.md so it has an Examples section that points to pyexamples and synthetic.

--- pyexamples/k_multi_table_lsh_example.py
+++ pyexamples/k_multi_table_lsh_example.py
@@ -52,38 +52,83 @@ def main():
     t.fill(data, False)
 
     k_probe_recall = []
+    k_probe_approx_recall = []
 
     # track the total time to perform all queries.
     exact_total_time = 0
     probe_total_time = 0
+    probe_approx_total_time = 0
 
     # "multiprobe" query the table.
     for query in queries:
-        # find the nearset neighbor and its distance
+        # find the true topk nearset neighbor and their distances
+        # get indices of the true topk to compute recall.
         start = time.time()
         indices, nns, dists = exact_k_neighbor(args.k, query, data)
         end = time.time()
         exact_total_time += (end - start)
+
         # probe the table and record the time.
         start = time.time()
         # the probe returns an option of KV, so it is MAYBE the
         # neighbor and its id; but, it could be None.
-        maybe_neighbors, stats = t.k_probe(5, query, args.adj)
+        maybe_neighbors, stats = t.k_probe(args.k, query, args.adj)
         end = time.time()
         probe_total_time += (end - start)
 
-        neighbors = list(list(zip(*maybe_neighbors))[1])
-        rec1 = recall(neighbors, indices)
+        # compute revall if no neighbors were found, recall is 0
+        if maybe_neighbors is None:
+            rec1 = 0
+        else:
+            neighbors = list(list(zip(*maybe_neighbors))[1])
+            rec1 = recall(neighbors, indices)
         k_probe_recall.append(rec1)
+
+        # find the first k items within distance c from the query
+        start = time.time()
+        maybe_neighbors, stats = t.k_probe_approx(args.k, query, 2.0, args.adj)
+        end = time.time()
+        probe_approx_total_time += (end - start)
+
+        # compute the recall. again, if no neighbors found, recall is 0.
+        if maybe_neighbors is None:
+            rec2 = 0
+        else:
+            neighbors = list(list(zip(*maybe_neighbors))[1])
+            rec2 = recall(neighbors, indices)
+        k_probe_approx_recall.append(rec2)
 
     print('Exact Average Time Per Query: ' +
           str(exact_total_time / args.num_queries))
     print('Probe Average Time Per Query: ' +
           str(probe_total_time / args.num_queries))
+    print('Probe Average Time Per Query: ' +
+          str(probe_approx_total_time / args.num_queries))
 
     print('mean recalls:')
     print(' * k_probe:        {0:.2f}'.format(
         sum(k_probe_recall) / len(k_probe_recall)))
+    print(' * k_probe_appeox:        {0:.2f}'.format(
+        sum(k_probe_approx_recall) / len(k_probe_approx_recall)))
+
+
+def generate_data(count, dim):
+    return np.random.randn(count, dim)
+
+
+def exact_k_neighbor(k, query, data):
+    # sort by distance and retrieve the indices of the k smallest
+    dists_from_query = np.linalg.norm(data - query, axis=1)
+    topk_indices = dists_from_query.argsort()[:k]
+    topk = data[topk_indices]
+    return topk_indices, topk, np.linalg.norm(topk - query)
+
+
+def vect_in(vect, d):
+    for row in d:
+        if np.array_equal(vect, row):
+            return True
+    return False
 
 
 def generate_data(count, dim):
